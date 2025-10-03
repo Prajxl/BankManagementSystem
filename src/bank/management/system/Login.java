273c170378b7a170fd013f2635f37d62a84b94dd
Prajwal K <154422749+Prajxl@users.noreply.github.com>
2025-10-03 19:47:22 +0530

    Added labels

--- src/bank/management/system/Login.java
+++ src/bank/management/system/Login.java
@@ -6,6 +6,11 @@ import java.awt.*;
 
 public class Login extends JFrame {
 
+    JLabel label1,label2,label3;
+    JTextField textField2;
+    JPasswordField passwordField3;
+
+
     Login()
     {
         super("Bank Management System");
@@ -24,12 +29,52 @@ public class Login extends JFrame {
         iimage.setBounds(630,350,100,100);
         add(iimage);
 
+
+//JLabel is used to add texts fonts in the screen inside the constructor because it should visible when program is running
+        label1 = new JLabel("WELCOME TO ATM ");
+        label1.setForeground(Color.WHITE);
+        label1.setFont(new Font("AvantGarde",Font.BOLD,38));
+        label1.setBounds(230,125,450,40);
+        add(label1);
+
+        label2 = new JLabel("Card No :");
+        label2.setFont(new Font("Ralway",Font.BOLD,28));
+        label2.setForeground(Color.WHITE);
+        label2.setBounds(150,190,375,30);
+        add(label2);
+
+//        JTextField is used to add textfield infront of card No:
+
+
+
+
+
+
+
+
+
+        label3 = new JLabel("Pin :");
+        label3.setFont(new Font("Ralway",Font.BOLD,28));
+        label3.setForeground(Color.WHITE);
+        label3.setBounds(150,250,375,30);
+        add(label3);
+
+
+
+
+
+
+
         ImageIcon iii1 = new ImageIcon(ClassLoader.getSystemResource("icon/backbg.png"));
         Image iii2 = iii1.getImage().getScaledInstance(850,480, Image.SCALE_DEFAULT);
         ImageIcon iii3 = new ImageIcon(iii2);
         JLabel iiimage = new JLabel(iii3);
         iiimage.setBounds(0,0,850,480);
         add(iiimage);
+
+
+
+
 
 
 
