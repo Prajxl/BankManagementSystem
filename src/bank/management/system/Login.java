7b6963c4d4d02c012e118b29ac4327f2b650e48a
Prajwal K <154422749+Prajxl@users.noreply.github.com>
2025-10-04 08:31:53 +0530

    LOGIN PAGE IS COMPLETED

--- src/bank/management/system/Login.java
+++ src/bank/management/system/Login.java
@@ -3,12 +3,17 @@ package bank.management.system;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Login extends JFrame {
+// ActionListener is a interface which is used to add Actions to the buttons
+// JFrame is for designing the frame
+public class Login extends JFrame implements ActionListener {
 
     JLabel label1,label2,label3;
     JTextField textField2;
     JPasswordField passwordField3;
+    JButton button1,button2,button3;
 
 
     Login()
@@ -45,7 +50,10 @@ public class Login extends JFrame {
 
 //        JTextField is used to add textfield infront of card No:
 
-
+        textField2 = new JTextField(15);
+        textField2.setBounds(300,193,230,30);
+        textField2.setFont(new Font("Arial",Font.BOLD,14));
+        add(textField2);
 
 
 
@@ -59,10 +67,34 @@ public class Login extends JFrame {
         label3.setBounds(150,250,375,30);
         add(label3);
 
+        passwordField3 = new JPasswordField(15);
+        passwordField3.setBounds(300,250,230,30);
+        passwordField3.setFont(new Font("Arial",Font.BOLD,14));
+        add(passwordField3);
 
+        button1 = new JButton("SIGN IN");
+        button1.setFont(new Font("Arial",Font.BOLD,14));
+        button1.setForeground(Color.WHITE);
+        button1.setBackground(Color.BLACK);
+        button1.setBounds(300,300,100,30);
+        button1.addActionListener(this);
+        add(button1);
 
+        button2 = new JButton("CLEAR");
+        button2.setFont(new Font("Arial",Font.BOLD,14));
+        button2.setForeground(Color.WHITE);
+        button2.setBackground(Color.BLACK);
+        button2.setBounds(430,300,100,30);
+        button2.addActionListener(this);
+        add(button2);
 
-
+        button3 = new JButton("SIGN UP");
+        button3.setFont(new Font("Arial",Font.BOLD,14));
+        button3.setBackground(Color.BLACK);
+        button3.setBounds(363,340,100,30);
+        button3.setForeground(Color.WHITE);
+        button3.addActionListener(this);
+        add(button3);
 
 
         ImageIcon iii1 = new ImageIcon(ClassLoader.getSystemResource("icon/backbg.png"));
@@ -83,6 +115,26 @@ public class Login extends JFrame {
         setSize(850,480);
         setVisible(true);
         setLocation(400,205);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try{
+            if(e.getSource() == button1)
+            {
+
+            } else if (e.getSource()== button2) {
+
+                textField2.setText("");
+                passwordField3.setText("");
+
+            }else {
+
+            }
+        } catch (Exception E) {
+            E.printStackTrace();
+        }
 
     }
 
